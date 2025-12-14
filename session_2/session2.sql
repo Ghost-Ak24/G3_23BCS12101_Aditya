58c8297a174ac75655c359b7b6dd421fc64b4160
Ghost-Ak24 <adityakapoor2408@gmail.com>
2025-12-14 23:59:24 +0530

    Create session2.sql

---
+++ session_2/session2.sql
@@ -0,0 +1,14 @@
+-- G3_23BCS12101_Aditya_Kapoor_session2
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+    IF N < 1 THEN
+        RETURN;
+    END IF;
+    RETURN QUERY (
+        -- Write your PostgreSQL query statement below.
+            select distinct e.salary from Employee as e
+            order by e.salary desc
+            limit 1 offset n-1  
+  );
+END;
+$$ LANGUAGE plpgsql;
